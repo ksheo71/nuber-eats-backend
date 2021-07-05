844f7f32e89dc76f3834e6990ac2731e26b37676
ksheo71 <kyoungseok.heo@gmail.com>
2021-07-06 08:41:16 +0900

    20210706

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,24 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    RestaurantsModule,
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
-    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'kyoungseok.heo',
+      password: '1234',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [],
   providers: [],
